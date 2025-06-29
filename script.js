880d0bc6fd84ed46f2fe721191e7969b8af8a2f7
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-06-29 08:20:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+//your JS code here. If required
+
+let obj = {
+	name: "Dhruw"
+}
+
+let arr = [];
+function getKeys(obj){
+	for(keys in obj){
+		arr.push(keys);
+	}
+	return arr;
+}
+
+getKeys(obj);
